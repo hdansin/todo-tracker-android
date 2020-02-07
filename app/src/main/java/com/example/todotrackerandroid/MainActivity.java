d1dec7085d08688ebf2653330c6e367338cefa53
hdansin <hdansin@mailbox.org>
2020-02-07 06:08:08 -0500

    init

---
+++ app/src/main/java/com/example/todotrackerandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.todotrackerandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.auth0.android.Auth0;
+
+public class MainActivity extends AppCompatActivity {
+    private Auth0 auth0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.logout);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        //Obtain the token from the Intent's extras
+        String accessToken = getIntent().getStringExtra(LoginActivity.EXTRA_ACCESS_TOKEN);
+        TextView textView = findViewById(R.id.credentials);
+        textView.setText(accessToken);
+    }
+
+    private void logout() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(LoginActivity.EXTRA_CLEAR_CREDENTIALS, true);
+        startActivity(intent);
+        finish();
+    }
+}
